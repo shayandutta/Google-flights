d89bf8a6e665431c31d2614865b8cf7d5edc39e2
shayandutta <shayandutta23@gmail.com>
2026-01-17 04:29:25 +0530

    feat: added helper utility to compare departureTime and arrivalTime for flights

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,14 +1,22 @@
 const { StatusCodes } = require("http-status-codes");
 const { FlightRepository } = require("../repositories");
 const AppError = require("../utils/errors/app-error");
+const { compareTime } = require("../utils/helpers/dateTimeHelpers");
 
 const flightRepository = new FlightRepository();
 
 async function createFlight(data){
     try{
+        if(compareTime(data.departureTime, data.arrivalTime)){
+            throw new AppError('Departure time cannot be greater than arrival time', StatusCodes.BAD_REQUEST)
+        }
         const response = await flightRepository.create(data);
         return response;
     }catch(error){
+        //If its already an AppError(if the error is caught in the try block and its an instance of AppError class), rethrow it as it is
+        if(error instanceof AppError){
+            throw error;
+        }
         if(error.name == "SequelizeValidationError" || error.name == "SequelizeUniqueConstraintError"){
             let explanation = [];
             error.errors.forEach((err)=>{
